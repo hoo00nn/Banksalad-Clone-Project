b4320c20febf213e5d81ddd22d863c22c6247892
hoo00nn <hoo00nn@naver.com>
2020-09-28 17:51:20 +0900

    Chore: dotenv 패키지 설치
    환경변수 관리하기 위해 dotenv 설치

--- server/app.js
+++ server/app.js
@@ -3,8 +3,9 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const app = express();
+require('dotenv').config();
 
-const api = require('./routes/api');
+const apiRouter = require('./routes/api');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -12,6 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client/src')));
 
-app.use('/api', api);
+app.use('/api', apiRouter);
 
 module.exports = app;

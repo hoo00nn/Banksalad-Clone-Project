ef368efd9d7e86bb4793caf1328fc3b1dd6b228b
hoo00nn <hoo00nn@naver.com>
2020-10-06 23:27:12 +0900

    Refactor: 파일 이름 변경 (user -> login)
    user.js안에 login 관련 api 밖에 존재하지 않아서 login.js로 이름 변경

--- server/src/routes/api/login.js
+++ server/src/routes/api/login.js
@@ -3,6 +3,6 @@ const express = require('express');
 const router = express.Router();
 const { loginAuth } = require('../../middlewares/auth');
 
-router.post('/login', loginAuth);
+router.post('/', loginAuth);
 
 module.exports = router;

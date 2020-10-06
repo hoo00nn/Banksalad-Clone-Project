e3231185447e777736e2763d1b9ae6faffd385ec
hoo00nn <hoo00nn@naver.com>
2020-10-06 11:09:35 +0900

    Refactor: 라우터에서 사용하는 미들웨어 구조 변경
    각 API 요청 라우터마다 등록했던 미들웨어를 api 주소로 들어오는 모든 url이 api인증 미들웨어를 거치도록 변경

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 const user = require('./api/user');
 const transaction = require('./api/transaction');
+const { apiAuth } = require('../middlewares/auth');
+
+router.use(apiAuth);
 
 router.use('/user', user);
 router.use('/transaction', transaction);

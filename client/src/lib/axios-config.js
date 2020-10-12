529730b6ffce88a7544eff7174e750c5b34809f8
hoo00nn <hoo00nn@naver.com>
2020-10-12 21:58:59 +0900

    Feat: axios content-type header 추가

--- client/src/lib/axios-config.js
+++ client/src/lib/axios-config.js
@@ -1,6 +1,8 @@
 export default {
-  baseURL: 'API_HOST',
+  baseURL: process.env.API_HOST,
   headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
     authorization: localStorage.getItem('authorization') || '',
   },
 };

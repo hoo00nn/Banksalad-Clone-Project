c7db3c10e6dc9afeb669e2ff46ba9b06cc8d4a34
hoo00nn <hoo00nn@naver.com>
2020-10-12 21:58:34 +0900

    Feat: login 인증 로직 상태 메세지 추가
    성공시 'succes', 실패시 'fail'을 리턴해주는 코드 추가

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -5,12 +5,12 @@ require('dotenv').config('../.env');
 const loginAuth = async (req, res, next) => {
   try {
     passport.authenticate('local', (error, user, { message } = '') => {
-      if (error || !user) return res.status(400).json({ message });
+      if (error || !user) return res.status(400).json({ status: 'fail', message });
 
       const payload = { no: user.no, id: user.user_id };
       const generateJWTToken = jwt.sign(payload, process.env.JWT_SECRET_KEY);
 
-      return res.json({ JWT: generateJWTToken });
+      return res.json({ status: 'success', JWT: generateJWTToken });
     })(req, res, next);
   } catch (err) {
     next(err);
@@ -20,7 +20,7 @@ const loginAuth = async (req, res, next) => {
 const apiAuth = async (req, res, next) => {
   try {
     passport.authenticate('jwt', { session: false }, (error, user, { message } = '') => {
-      if (error || !user) res.status(400).json({ message });
+      if (error || !user) res.status(400).json({ status: 'fail', message });
 
       req.body.user_no = user.no;
       next();

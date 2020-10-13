11279877e3cd5d7ab7f856922117b692182dfc8e
hoo00nn <hoo00nn@naver.com>
2020-10-13 13:02:10 +0900

    Refactor: index.js 로직 수정
    - 토큰 검사를 한 후 토큰이 존재하면 가계부 페이지로, 존재하지 않으면 로그인 페이지로 이동하도록 변경

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,17 @@
 import '@styles/account-book.scss';
+import { isToken } from '@lib/token';
 import Login from '@views/login';
+import AccountBook from '@views/account-book';
 
-const main = new Login();
+const main = {
+  render: () => {
+    if (!isToken()) {
+      const loginPage = new Login();
+      return loginPage.render();
+    }
+    const accountBook = new AccountBook();
+    return accountBook.render();
+  },
+};
+
 main.render();

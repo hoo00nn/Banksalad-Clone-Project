aa058b784b8dfd2269fff9a5def5c4186e2d8bd2
hoo00nn <hoo00nn@naver.com>
2020-10-13 12:53:16 +0900

    Feat: token 관련 유틸 함수 추가
    - 토큰 관련 함수 따로 모듈로 관리 하기 위함
    - 토큰 저장, 토큰 조회, 토큰 존재여부 체크 함수 구현

---
+++ client/src/lib/token.js
@@ -0,0 +1,12 @@
+export const setToken = (token) => {
+  return localStorage.setItem('authorization', token);
+};
+
+export const getToken = () => {
+  return localStorage.getItem('authorization');
+};
+
+export const isToken = () => {
+  if (!localStorage.getItem('authorization')) return false;
+  return true;
+};
